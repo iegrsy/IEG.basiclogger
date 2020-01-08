e399da7a5655ba82f074dde4d26c1181e53521e4
Ibrahim Ethem Gursoy <ieg@bilkon-kontrol.com.tr>
2020-01-08 16:28:32 +0300

    Add thread safe logger

--- Logger.cs
+++ Logger.cs
@@ -1,48 +1,98 @@
 using System;
+using System.Collections.Concurrent;
+using System.Threading;
 
-public class Logger
+namespace IEG
 {
-    private static readonly string _FILE_NAME = ".log.log";
-
-    private static string GetFormattedOut(string s, string t, string tag)
+    public class Logger : IDisposable
     {
-        return $"{DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss.fff")}\t{t}\t{tag}\t[{System.Threading.Thread.CurrentThread.ManagedThreadId}]\t{s}\n";
-    }
+        private static readonly string _FILE_NAME = ".log.log";
 
-    public string TAG { get; private set; }
+        private static string GetFormattedOut(string s, string t, string tag)
+        {
+            return $"{DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss.fff")}\t{t}\t{tag}\t[{System.Threading.Thread.CurrentThread.ManagedThreadId}]\t{s}\n";
+        }
 
-    public bool IsLog { get; set; }
+        public string TAG { get; private set; }
 
-    public Logger(string tag)
-    {
-        TAG = tag;
-    }
+        public bool IsLog { get; set; }
 
-    public void Debug(string s)
-    {
-        string l = GetFormattedOut(s, "DEBUG", TAG);
-        if (IsLog) System.IO.File.AppendAllText(_FILE_NAME, l);
-        System.Console.WriteLine(l);
-    }
+        public Logger(string tag)
+        {
+            TAG = tag;
+        }
 
-    public void Info(string s)
-    {
-        string l = GetFormattedOut(s, "INFO", TAG);
-        if (IsLog) System.IO.File.AppendAllText(_FILE_NAME, l);
-        System.Console.WriteLine(l);
-    }
+        public void Debug(string s)
+        {
+            string l = GetFormattedOut(s, "DEBUG", TAG);
+            if (IsLog) Write2File(l);
+            System.Console.WriteLine(l);
+        }
 
-    public void Warn(string s)
-    {
-        string l = GetFormattedOut(s, "WARN", TAG);
-        if (IsLog) System.IO.File.AppendAllText(_FILE_NAME, l);
-        System.Console.WriteLine(l);
-    }
+        public void Info(string s)
+        {
+            string l = GetFormattedOut(s, "INFO", TAG);
+            if (IsLog) Write2File(l);
+            System.Console.WriteLine(l);
+        }
 
-    public void Error(string s, Exception e)
-    {
-        string l = GetFormattedOut(s, "ERROR", TAG);
-        if (IsLog) System.IO.File.AppendAllText(_FILE_NAME, l);
-        System.Console.WriteLine(l, e);
+        public void Warn(string s)
+        {
+            string l = GetFormattedOut(s, "WARN", TAG);
+            if (IsLog) Write2File(l);
+            System.Console.WriteLine(l);
+        }
+
+        public void Error(string s, Exception e)
+        {
+            string l = GetFormattedOut(s, "ERROR", TAG);
+            if (IsLog) Write2File(l);
+            System.Console.WriteLine(l, e);
+        }
+
+        private ConcurrentQueue<string> logQ = new ConcurrentQueue<string>();
+
+        private void Write2File(string msg)
+        {
+            if (msg == null || String.IsNullOrEmpty(msg))
+                return;
+
+            AddLog(msg);
+        }
+
+        private void AddLog(string log)
+        {
+            logQ.Enqueue(log);
+            if (logQ.Count > 10)
+                CleanQ();
+        }
+
+        private static readonly object _lock = new object();
+        private void CleanQ()
+        {
+            if (logQ?.Count <= 0)
+                return;
+
+            new Thread(() =>
+            {
+                try
+                {
+                    string log;
+                    while (logQ.TryDequeue(out log))
+                    {
+                        lock (_lock)
+                        {
+                            System.IO.File.AppendAllText(_FILE_NAME, log);
+                        }
+                    }
+                }
+                catch (System.Exception) { }
+            }).Start();
+        }
+
+        public void Dispose()
+        {
+            CleanQ();
+        }
     }
 }
